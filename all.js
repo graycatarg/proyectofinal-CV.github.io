58c25b41b83b67833f5950f668c614ef7fc03bb6
graycatarg <126100589+graycatarg@users.noreply.github.com>
2023-03-02 15:47:00 -0300

    Add files via upload

---
+++ all.js
@@ -0,0 +1,9 @@
+
+document.getElementById("boton").onclick=function(){
+    console.log("capturamoseleventoclick")
+}
+
+var btn = document.querySelector("button")
+btn.onclick = () => {
+    window.print(); 
+}
